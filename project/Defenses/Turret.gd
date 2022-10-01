a05fa3e2f623c507252704c0baf5532d4f50f312
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-09-30 20:24:22 -0400

    Turn the turrets on only one axis

--- project/Defenses/Turret.gd
+++ project/Defenses/Turret.gd
@@ -5,7 +5,10 @@ var _tracked_enemy : Spatial
 
 func _physics_process(_delta):
 	if _tracked_enemy:
-		look_at(_tracked_enemy.global_translation, Vector3.UP)
+		var target := _tracked_enemy.global_translation
+		var source := global_translation
+		# This will turn to face the target, only turning on the y axis.
+		global_rotation.y = lerp_angle(global_rotation.y, atan2(source.x - target.x, source.z - target.z), 1)
 
 
 func _on_Area_body_entered(body):
