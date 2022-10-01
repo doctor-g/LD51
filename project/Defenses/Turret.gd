ed4c4690366a00c916904007c3bfd5fee1947451
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-01 15:56:15 -0400

    Add shooting sound

--- project/Defenses/Turret.gd
+++ project/Defenses/Turret.gd
@@ -3,8 +3,12 @@ extends Spatial
 export var dps := 20.0
 
 var _tracked_enemy : Spatial
+var _random_generator := RandomNumberGenerator.new()
+
 
 onready var _particles := $CPUParticles
+onready var _shoot_sound := $ShootSound
+onready var _shoot_sound_timer := $ShootSoundTimer
 
 
 func _physics_process(delta):
@@ -20,9 +24,21 @@ func _physics_process(delta):
 func _on_Area_body_entered(body):
 	_tracked_enemy = body
 	_particles.emitting = true
-
+	_play_shooting_sound()
+	
 
 func _on_Area_body_exited(body):
 	if body==_tracked_enemy:
 		_tracked_enemy = null
 		_particles.emitting = false
+
+
+func _on_ShootSoundTimer_timeout():
+	_play_shooting_sound()
+
+
+func _play_shooting_sound():
+	if _particles.emitting:
+		_shoot_sound.pitch_scale = _random_generator.randfn(1, 0.1)
+		_shoot_sound.play()
+		_shoot_sound_timer.start(1.0/8.0)
