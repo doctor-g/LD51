3365b20419c40834f56b5fe386afa5f532edb1a9
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-01 18:14:24 -0400

    Play simple base animation on hit

--- project/Level/Base.gd
+++ project/Level/Base.gd
@@ -1,10 +1,12 @@
 extends Spatial
 
 onready var _base_hit_sound := $BaseHitSound
+onready var _anim_player := $AnimationPlayer
 
 func _on_Area_body_entered(body):
 	Global.base_health -= 1
 	_base_hit_sound.play()
+	_anim_player.play("Hit")
 	body.queue_free()
 	
 	if Global.base_health <= 0:
