8beabbdc537f94c43ae0344dab3978338ad973fb
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-01 07:22:14 -0400

    Add base
    The map was changed to 11x11 so that the base could be centered.

--- project/Level/Level.gd
+++ project/Level/Level.gd
@@ -2,35 +2,69 @@ extends Spatial
 tool
 
 const _Tile := preload("res://Level/Tile.tscn")
+const _PATH := [
+	Vector2(0,9),
+	Vector2(1,9),
+	Vector2(2,9),
+	Vector2(3,9),
+	Vector2(4,9),
+	Vector2(5,9),
+	Vector2(6,9),
+	Vector2(7,9),
+	Vector2(8,9),
+	Vector2(9,9),
+	Vector2(9,8),	
+	Vector2(9,7),
+	Vector2(9,6),
+	Vector2(9,5),
+	Vector2(8,5),
+	Vector2(7,5),
+	Vector2(6,5),
+	Vector2(5,5),
+	Vector2(5,4),
+	Vector2(5,3),
+	Vector2(5,2),
+	Vector2(5,1),
+	Vector2(5,0),
+	Vector2(5,-1)
+]
 
-export var width := 10
-export var height := 10
+const width := 11
+const height := 11
 
 func _enter_tree():
 	# warning-ignore:integer_division
 	var min_x := -width / 2
-	# warning-ignore:integer_division	
-	var max_x := width / 2
-	# warning-ignore:integer_division	
+	# warning-ignore:integer_division
 	var min_z := -height / 2
-	# warning-ignore:integer_division	
-	var max_z := height / 2
 	
 	for i in width:
 		for j in height:
-			var box : Spatial = _Tile.instance()
-			add_child(box)
-			box.translation = Vector3(lerp(min_x, max_x, float(i)/width), 0, lerp(min_z, max_z, float(j)/height))
-			if j == 5:
-				box.selectable = false
+			var tile : Spatial = _Tile.instance()
+			add_child(tile)
+			tile.translation = Vector3(min_x+i, 0, min_z+j)
+	#		print(tile.translation)
+			if _PATH.find(Vector2(i,j)) >= 0:
+				tile.selectable = false
+				
+	var base : Spatial = preload("res://Level/Base.tscn").instance()
+	base.translation= Vector3(0, 1, min_z-1)
+	add_child(base)
 	
 
 func _ready():
 	var path := Path.new()
-	path.translation = Vector3(-5,1,0)
+	
+	# Put the path node in the upper-left corner 
+	# so that it will account from the _PATH coordinates
+	# and just high enough that the baseline of enemies is on top of the ground.
+	#
+	# warning-ignore:integer_division
+	# warning-ignore:integer_division	
+	path.translation = Vector3(-width/2, 1 ,-height/2)
+	for point in _PATH:
+		path.curve.add_point(Vector3(point.x, 0, point.y))
 	add_child(path)
-	path.curve.add_point(Vector3.ZERO)
-	path.curve.add_point(Vector3(10,0,0))
 	var path_follow := PathFollow.new()
 	path.add_child(path_follow)
 	var sphere :Spatial = preload("res://Enemies/Sphere.tscn").instance()
