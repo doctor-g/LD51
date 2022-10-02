2ae996381b612826b8956f690cf09732cea041ce
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-01 20:53:18 -0400

    Apply random rotation on beamed shards

--- project/Beam/Beam.gd
+++ project/Beam/Beam.gd
@@ -4,21 +4,25 @@ const _SHARD_PICKUP_HEIGHT = 3
 const _SHARD_PICKUP_SPEED = 1.5
 const _Particles := preload("res://Beam/CollectionParticles.tscn")
 
-var _attracting_shards := []
+var _attracting_shards := {}
 
 onready var _sound := $BeamSound
 
 func _on_Beam_body_entered(body:RigidBody):
-	if body is Shard and _attracting_shards.find(body)==-1:
-		_attracting_shards.append(body)
+	if body is Shard and not _attracting_shards.has(body):
+		_attracting_shards[body] = _random_vector()
 		body.mode = RigidBody.MODE_KINEMATIC
 		_sound.play()
 
 
+func _random_vector()->Vector3:
+	return Vector3(randf(), randf(), randf()).normalized()
+
+
 func _on_Beam_body_exited(body:RigidBody):
-	var index = _attracting_shards.find(body)
-	if body is Shard and index != -1:
-		_attracting_shards.remove(index)
+	if body is Shard and _attracting_shards.has(body):
+		# warning-ignore:return_value_discarded
+		_attracting_shards.erase(body)
 		body.mode = RigidBody.MODE_RIGID
 	
 	if _attracting_shards.size()==0:
@@ -28,6 +32,8 @@ func _on_Beam_body_exited(body:RigidBody):
 func _physics_process(delta):
 	for shard in _attracting_shards:
 		shard.global_translate(Vector3.UP * _SHARD_PICKUP_SPEED * delta)
+		shard.rotate(_attracting_shards[shard], PI*delta)
+		
 		if shard.global_translation.y >= _SHARD_PICKUP_HEIGHT:
 			var particles = _Particles.instance()
 			add_child(particles)
