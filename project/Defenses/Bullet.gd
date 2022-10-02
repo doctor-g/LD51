a10a6d0d39a7847f817ffa46d9f415e274f70c85
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-01 20:18:33 -0400

    Replace particle bullets with kinematic ones

---
+++ project/Defenses/Bullet.gd
@@ -0,0 +1,18 @@
+extends KinematicBody
+
+export var damage := 10
+
+const _SPEED := 10
+
+
+
+func _physics_process(delta):
+	var velocity = (Vector3.FORWARD * _SPEED).rotated(Vector3.UP, global_rotation.y)
+	var collision := move_and_collide(velocity*delta)
+	if collision:
+		collision.collider.damage(damage)
+		queue_free()
+
+
+func _on_LifetimeTimer_timeout():
+	queue_free()
--- project/Defenses/Turret.gd
+++ project/Defenses/Turret.gd
@@ -2,35 +2,52 @@ extends Spatial
 
 export var dps := 20.0
 
+# Bullets per second
+export var firing_rate := 3.0
+
 var _tracked_enemy : Spatial
 var _random_generator := RandomNumberGenerator.new()
+var _shooting := false
 
 
-onready var _particles := $CPUParticles
 onready var _shoot_sound := $ShootSound
-onready var _shoot_sound_timer := $ShootSoundTimer
+onready var _bullet_spawn_point := $BulletSpawnPoint
+onready var _shot_timer := $ShotTimer
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if _tracked_enemy:
-		var target := _tracked_enemy.global_translation
-		var source := global_translation
-		# This will turn to face the target, only turning on the y axis.
-		global_rotation.y = lerp_angle(global_rotation.y, atan2(source.x - target.x, source.z - target.z), 1)
+		_turn_toward_tracked_enemy()
 		
-		_tracked_enemy.damage(dps * delta)
+
+func _turn_toward_tracked_enemy()->void:
+	var target := _tracked_enemy.global_translation
+	var source := global_translation
+	# This will turn to face the target, only turning on the y axis.
+	global_rotation.y = lerp_angle(global_rotation.y, atan2(source.x - target.x, source.z - target.z), 1)
 
 
 func _on_Area_body_entered(body):
 	_tracked_enemy = body
-	_particles.emitting = true
-	_play_shooting_sound()
+	_turn_toward_tracked_enemy()
+	_shooting = true
+	_shoot()
 	
+
+func _shoot()->void:
+	var bullet :Spatial= preload("res://Defenses/Bullet.tscn").instance()
+	add_child(bullet)
+	bullet.global_translation = _bullet_spawn_point.global_translation
+	bullet.global_rotation = _bullet_spawn_point.global_rotation
+	bullet.set_as_toplevel(true)
+	_play_shooting_sound()
+	_shot_timer.start(1.0/firing_rate)
+
 
 func _on_Area_body_exited(body):
 	if body==_tracked_enemy:
 		_tracked_enemy = null
-		_particles.emitting = false
+		_shooting = false
 
 
 func _on_ShootSoundTimer_timeout():
@@ -38,7 +55,10 @@ func _on_ShootSoundTimer_timeout():
 
 
 func _play_shooting_sound():
-	if _particles.emitting:
-		_shoot_sound.pitch_scale = _random_generator.randfn(1, 0.1)
-		_shoot_sound.play()
-		_shoot_sound_timer.start(1.0/8.0)
+	_shoot_sound.pitch_scale = _random_generator.randfn(1, 0.1)
+	_shoot_sound.play()
+
+
+func _on_ShotTimer_timeout():
+	if _shooting:
+		_shoot()
