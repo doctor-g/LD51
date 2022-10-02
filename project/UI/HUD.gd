1fe9b21417f42a475be4b4e7bbc80e40f6e79232
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-02 14:26:22 -0400

    Darken resources when insufficient for a turret

--- project/UI/HUD.gd
+++ project/UI/HUD.gd
@@ -18,6 +18,11 @@ func _ready():
 func _process(_delta):
 	_displayed_score = lerp(_displayed_score, Global.score, lerp_weight)
 	_displayed_resources = lerp(_displayed_resources, Global.resources, lerp_weight)
+
+	if Global.resources < 300:
+		_resources_label.add_color_override("font_color", Color.darkslateblue)
+	else:
+		_resources_label.remove_color_override("font_color")
 	
 	_score_label.text = "Score: %d" % ceil(_displayed_score)
 	_resources_label.text = "Resources: %d" % round(_displayed_resources)
