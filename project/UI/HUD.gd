65b4ce0a23df983d12d15f3b552a329306ca5e76
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-02 15:15:08 -0400

    Show meteor countdown in HUD
    This is less elegant, but it serves the purpose and saves space

--- project/UI/HUD.gd
+++ project/UI/HUD.gd
@@ -8,14 +8,11 @@ var _displayed_resources : float
 onready var _score_label := find_node("ScoreLabel")
 onready var _resources_label := find_node("ResourcesLabel")
 onready var _base_health_label := find_node("BaseHealthLabel")
-onready var _meteor_bar := find_node("MeteorProgressBar")
-onready var _anim_player := $AnimationPlayer
-
+onready var _countdown := find_node("Countdown")
 
 func _ready():
 	_displayed_score = 0
 	_displayed_resources = Global.DEFAULT_RESOURCES
-
 
 func _process(_delta):
 	_displayed_score = lerp(_displayed_score, Global.score, lerp_weight)
@@ -29,6 +26,5 @@ func _process(_delta):
 	_score_label.text = "Score: %d" % ceil(_displayed_score)
 	_resources_label.text = "Resources: %d" % round(_displayed_resources)
 	_base_health_label.text = "Base Health: %d" % Global.base_health
-	_meteor_bar.value = Global.time_to_impact
 	
-	_anim_player.play("Warning" if Global.time_to_impact >= 9.0 else "RESET")
+	_countdown.text = "Next Meteor: %d" % (10-Global.time_to_impact)
