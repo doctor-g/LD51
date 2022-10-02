549f54b6d82b20abc042f4c5fe4f7ac397be1a67
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-10-02 13:12:40 -0400

    Pulse sphere to show damage sustained

--- project/Enemies/Sphere.gd
+++ project/Enemies/Sphere.gd
@@ -2,14 +2,19 @@ extends Spatial
 
 signal destroyed(position)
 
+export var max_health := 100.0
 export var health := 100.0
 export var points := 100
 export var resources := 100
 
 var _destroyed := false
 
+
 func damage(amount:float)->void:
 	health -= amount
+	
+	var pulse_speed :float=  lerp(10.0, 1.0, health / max_health)
+	$CSGSphere.material.set_shader_param("pulse_speed", pulse_speed)
 	
 	# This thing can be damaged more than once per frame,
 	# so we have to separately keep track of whether it was
